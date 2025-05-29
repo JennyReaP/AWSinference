ee82f4cd141b2f15165ef5787ef66e68238648f1
JennyReaP <jenny.rea@yachaytech.edu.ec>
2025-05-28 20:57:49 -0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,7 +3,7 @@ import joblib
 # Datos fijos para prueba
 features = [[1, 45, 58, 11, 22, 0]]
 # Cargar el modelo
-modelo = joblib.load("modeloPinguino.pkl")
+modelo = joblib.load("modelo.pkl")
 # Hacer la predicción
 prediccion = modelo.predict(features)
 print("Predicción:", prediccion[0])

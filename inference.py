7eb544f67026ef1699749447304327a6631ca1d6
JennyReaP <jenny.rea@yachaytech.edu.ec>
2025-05-28 20:52:59 -0500

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,9 @@
+import joblib
+
+# Datos fijos para prueba
+features = [[1, 45, 58, 11, 22, 0]]
+# Cargar el modelo
+modelo = joblib.load("modeloPinguino.pkl")
+# Hacer la predicción
+prediccion = modelo.predict(features)
+print("Predicción:", prediccion[0])
